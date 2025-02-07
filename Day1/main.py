318da819e99b35c00f77d8bb4570968baa39ddaa
jade fisher <fisherrjd@gmail.com>
2025-02-06 18:20:03 -0700

    part 2 of day 1 completed

--- Day1/main.py
+++ Day1/main.py
@@ -1,21 +1,32 @@
-with open("input", "r") as file:
+from collections import Counter
+
+with open("Inputs/input", "r") as file:
     left_list = []
     right_list = []
 
     for line in file:
-        left,right = map(int, line.split())
+        left, right = map(int, line.split())
         left_list.append(left)
         right_list.append(right)
 
-print("Left List:", left_list)
-print("Right List:", right_list)
 
-left_list.sort()
-right_list.sort()
+def part1(left_list, right_list):
+    left_list.sort()
+    right_list.sort()
+    total_distance = sum(abs(l - r) for l, r in zip(left_list, right_list))
+    print("Total Distance:", total_distance)
+    return total_distance
 
-print("Left List:", left_list)
-print("Right List:", right_list)
+
+def part2(left_list, right_list):
+    right_count = Counter(right_list)
+
+    total_sum = sum(value * right_count[value] for value in left_list)
+    print("Similarity score:", +total_sum)
+    return total_sum
+
+
+sum_1 = part1(left_list, right_list)
+sum_2 = part2(left_list, right_list)
 
 # TODO: Learn about ZIP
-total_distance = sum(abs(l - r) for l, r in zip(left_list, right_list))
-print("Total Distance:", total_distance)
