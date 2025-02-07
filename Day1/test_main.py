53b355642b6c61a02fb1b85aa5bbc408019bad73
jade fisher <fisherrjd@gmail.com>
2025-02-07 13:54:46 -0700

    guh

--- Day1/test_main.py
+++ Day1/test_main.py
@@ -14,10 +14,12 @@ with open("./Day1/Inputs/input", "r") as file:
 def test_part1():
     expected = 2344935
     result = part1(left_list, right_list)
+    print(result)
     assert result == expected, f"Expected {expected}, got {result}"
 
 
 def test_part2():
     expected = 27647262
     result = part2(left_list, right_list)
+    print(result)
     assert result == expected, f"Expected {expected}, got {result}"
