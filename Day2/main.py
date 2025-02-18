2a35c0c832d3996d86d8b346c6272c9ac7974932
jade fisher <fisherrjd@gmail.com>
2025-02-17 19:36:48 -0700

    fix reading inputs and init the test cases

--- Day2/main.py
+++ Day2/main.py
@@ -1,20 +1,14 @@
+import os
+
+def part1():
+    
+
+
 # SMALL INPUT               There is 1 report per line
 # 7 6 4 2 1                 ~good~ IF
 # 1 2 7 8 9                 All increasing || Decreasing
-# 9 7 6 2 1                 rerport[n+1] +- 3 && != reprot[n]
+# 9 7 6 2 1                 report[n+1] +- 3 && != report[n]
 # 1 3 2 4 5
 # 8 6 4 4 1
 # 1 3 6 7 9
-
-
-# Ways to solve
-# Grab per report
-def part1(reports: list[list[int]]):
-    pass
-
-
-def part2(reports: list[list[int]]):
-    pass
-
-
-# def part1():
+# Call main() to execute the script
--- Day2/test_Day2.py
+++ Day2/test_Day2.py
@@ -1,10 +1,36 @@
 import pytest
+import os
+from main import part1
 
-with open("Day2/Inputs/input_small", "r") as file:
-    reports = []
 
-    for line in file:
-        print("line:", line.strip())  # Debugging
-        reports.append([int(x) for x in line.split()])
+def getReports():
+    file_path = os.path.join("Inputs", "input_small")
+    # Read and process the file
+    with open(file_path, "r") as file:
+        reports = []
 
-print("test:", reports)
+        for line in file:
+            stripped_line = line.strip()
+
+            # Debugging
+            print(stripped_line)
+
+            if not stripped_line:
+                continue
+
+            report = [int(x) for x in stripped_line.split()]
+            reports.append(report)
+    return reports
+
+
+def test_part1():
+    part1(getReports())
+    print("success")
+
+
+def test_part2():
+    print("success")
+
+
+test_part1()
+test_part2()
